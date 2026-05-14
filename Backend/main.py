06272ae8defe6059ae4f8ab2cfa0e89a4710faaf
shriman-patel <shrimanp304@gmail.com>
2026-05-15 01:05:07 +0530

    cors updated

--- Backend/main.py
+++ Backend/main.py
@@ -78,7 +78,7 @@ def get_db():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
